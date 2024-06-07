5bd451d10a36eced83fb33dbd043b4899117577d
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-06-07 15:52:23 -0400

    input some stylings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,20 @@ export default async function Home() {
     orderBy: { createdAt: "desc" },
   });
   return (
-    <main>
-      {jobs.map((job) => (
-        <JobListItem job={job} key={job.id} />
-      ))}
+    <main className="max-w-5xl m-auto px-3 my-10 space-y-10">
+      <div className="space-y-5 text-center">
+        <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
+          Developer Jobs
+        </h1>
+        <p className="text-muted-foreground">Find your dream jobs.</p>
+      </div>
+      <section>
+        <div className="space-y-4">
+          {jobs.map((job) => (
+            <JobListItem job={job} key={job.id} />
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
