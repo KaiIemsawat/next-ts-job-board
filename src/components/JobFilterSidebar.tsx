4c8fcf73e253f8635803d61ef2b42b165c9fc271
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-06-07 20:03:29 -0400

    select compontent (structure - no options)

---
+++ src/components/JobFilterSidebar.tsx
@@ -0,0 +1,27 @@
+import { Input } from "./ui/input";
+import { Label } from "./ui/label";
+import Select from "./ui/select";
+
+const filterJobs = async (formData: FormData) => {
+  "use server";
+};
+
+const JobFilterSidebar = () => {
+  return (
+    <aside className="md:w-[260px] p-4 sticky top-0 h-fit bg-background border rounded-lg">
+      <form action={filterJobs}>
+        <div className="space-y-4">
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="q">Search</Label>
+            <Input id="q" name="q" placeholder="Title, company, etc." />
+          </div>
+          <div className="flex flex-col gap-2">
+            <Label htmlFor="location">Location</Label>
+            <Select id="location" name="location"></Select>
+          </div>
+        </div>
+      </form>
+    </aside>
+  );
+};
+export default JobFilterSidebar;
