a439359f472c76d44345e9bbb136fb32f5b24d05
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-06-17 21:36:45 -0400

    not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import H1 from "@/components/ui/h1";
+
+const NotFound = () => {
+  return (
+    <main className="m-auto my-10 max-w-5xl space-y-5 px-3 text-center">
+      <H1>Not Found</H1>
+      <p>Sorry, the page you are trying to visit does not exist.</p>
+    </main>
+  );
+};
+export default NotFound;
