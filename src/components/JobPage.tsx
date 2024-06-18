f2927308ae9272acee7a31c81cf7bf04675991ae
Kai Iemsawat <kaiiemsawat@gmail.com>
2024-06-17 21:25:57 -0400

    added Markdown

--- src/components/JobPage.tsx
+++ src/components/JobPage.tsx
@@ -1,6 +1,9 @@
+import { formatMoney } from "@/lib/utils";
 import { Job } from "@prisma/client";
+import { Banknote, Briefcase, Globe2, MapPin } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import Markdown from "./Markdown";
 
 interface JobPageProps {
   job: Job;
@@ -31,24 +34,43 @@ const JobPage = ({
             className="rounded-xl"
           />
         )}
-      </div>
-      <div>
         <div>
-          <h1 className="text-xl font-bold">{title}</h1>
-          <p className="font-semibold">
-            {applicationUrl ? (
-              <Link
-                href={new URL(applicationUrl).origin}
-                className="text-green-500 hover:underline"
-              >
-                {companyName}
-              </Link>
-            ) : (
-              <span>{companyName}</span>
-            )}
-          </p>
+          <div>
+            <h1 className="text-xl font-bold">{title}</h1>
+            <p className="font-semibold">
+              {applicationUrl ? (
+                <Link
+                  href={new URL(applicationUrl).origin}
+                  className="text-green-500 hover:underline"
+                >
+                  {companyName}
+                </Link>
+              ) : (
+                <span>{companyName}</span>
+              )}
+            </p>
+          </div>
+          <div className="text-muted-foreground">
+            <p className="flex items-center gap-1.5">
+              <Briefcase size={16} className="shrink-0" />
+              {type}
+            </p>
+            <p className="flex items-center gap-1.5">
+              <MapPin size={16} className="shrink-0" />
+              {locationType}
+            </p>
+            <p className="flex items-center gap-1.5">
+              <Globe2 size={16} className="shrink-0" />
+              {location || "Worldwide"}
+            </p>
+            <p className="flex items-center gap-1.5">
+              <Banknote size={16} className="shrink-0" />
+              {formatMoney(salary)}
+            </p>
+          </div>
         </div>
       </div>
+      <div>{description && <Markdown>{description}</Markdown>}</div>
     </section>
   );
 };
